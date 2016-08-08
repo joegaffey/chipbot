8414c85fded89bcb0631b0db72bb5b0dd486c842
Joe Gaffey <joe@joegaffey.com>
2016-08-08 23:11:18 +0100

    Update bot-ctrl.sh

--- bot-ctrl.sh
+++ bot-ctrl.sh
@@ -26,7 +26,7 @@ do
      sh -c 'echo 0 > /sys/class/gpio/gpio410/value'
      sh -c 'echo 1 > /sys/class/gpio/gpio411/value'
   fi
-  if [ $c == ' ' ]; then
+  if [ $c == 'p' ]; then
     sh -c 'echo 0 > /sys/class/gpio/gpio408/value'
     sh -c 'echo 0 > /sys/class/gpio/gpio409/value'
     sh -c 'echo 0 > /sys/class/gpio/gpio410/value'
