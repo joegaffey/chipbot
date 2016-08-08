e5df757b041e9b19863a467d424fdf73e90c4c33
Joe Gaffey <joe@joegaffey.com>
2016-08-08 22:54:52 +0100

    Update setup-pins.sh

--- setup-pins.sh
+++ setup-pins.sh
@@ -4,9 +4,11 @@ sh -c 'echo out > /sys/class/gpio/gpio408/direction'
 sh -c 'echo out > /sys/class/gpio/gpio409/direction'
 sh -c 'echo out > /sys/class/gpio/gpio410/direction'
 sh -c 'echo out > /sys/class/gpio/gpio411/direction'
+sh -c 'echo out > /sys/class/gpio/gpio412/direction'
+sh -c 'echo in > /sys/class/gpio/gpio413/direction'
 
 sh -c 'echo 0 > /sys/class/gpio/gpio408/value'
 sh -c 'echo 0 > /sys/class/gpio/gpio409/value'
 sh -c 'echo 0 > /sys/class/gpio/gpio410/value'
 sh -c 'echo 0 > /sys/class/gpio/gpio411/value'
-
+sh -c 'echo 0 > /sys/class/gpio/gpio412/value'
